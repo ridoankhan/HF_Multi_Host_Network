442b71ea6e2753ebc60e227b7e46bab28e6a325a
Ridoan Khan Anik <ridoan.anik@bjitgroup.com>
2022-06-29 17:08:14 +0600

    fresh local start

--- FabricMultiHostDeployment/setup1/vm1/deployChaincode.sh
+++ FabricMultiHostDeployment/setup1/vm1/deployChaincode.sh
@@ -86,10 +86,11 @@ queryInstalled() {
 
 approveForMyOrg1() {
     setGlobalsForPeer0Org1
+    echo PackageID is ${PACKAGE_ID}
     # set -x
     # Replace localhost with your orderer's vm IP address
-    peer lifecycle chaincode approveformyorg -o localhost:7050 \
-        --ordererTLSHostnameOverride orderer.example.com --tls \
+    peer lifecycle chaincode approveformyorg -o 10.40.41.162:7050 \
+        --ordererTLSHostnameOverride 10.40.41.162 --tls \
         --cafile $ORDERER_CA --channelID $CHANNEL_NAME --name ${CC_NAME} --version ${VERSION} \
         --init-required --package-id ${PACKAGE_ID} \
         --sequence ${VERSION}
@@ -99,8 +100,8 @@ approveForMyOrg1() {
 
 }
 
-# queryInstalled
-# approveForMyOrg1
+queryInstalled
+approveForMyOrg1
 
 checkCommitReadyness() {
     setGlobalsForPeer0Org1
--- README.md
+++ README.md
@@ -10,3 +10,18 @@ vm2 - 10.40.41.164
 vm3 - 10.40.41.163
 
 vm4 - 10.40.41.162
+
+
+
+
+115.127.24.187
+
+115.127.24.183
+
+115.127.24.185
+
+115.127.24.188
+
+
+
+peer lifecycle chaincode approveformyorg -o 10.40.41.162:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile $ORDERER_CA --channelID $CHANNEL_NAME --name ${CC_NAME} --version ${VERSION} --init-required --package-id ${PACKAGE_ID} --sequence ${VERSION}
