94eb8fd10267be6b53ccb04d9d3ffa536879b51e
M Iffat Hossain <iffatshaon@gmail.com>
2022-06-30 16:09:39 +0600

    first working code from iffat-pc2

--- FabricMultiHostDeployment/setup1/vm2/createChannel.sh
+++ FabricMultiHostDeployment/setup1/vm2/createChannel.sh
@@ -1,59 +1,59 @@
 export CORE_PEER_TLS_ENABLED=true
 export ORDERER_CA=${PWD}/../vm4/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
-export PEER0_ORG2_CA=${PWD}/crypto-config/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+export PEER0_ORG1_CA=${PWD}/crypto-config/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
 export FABRIC_CFG_PATH=${PWD}/../../artifacts/channel/config/
 
 export CHANNEL_NAME=mychannel
 
-setGlobalsForPeer0Org2() {
+setGlobalsForPeer0Org1(){
     export CORE_PEER_LOCALMSPID="Org2MSP"
-    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG2_CA
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/crypto-config/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
     export CORE_PEER_ADDRESS=localhost:9051
-
 }
 
-setGlobalsForPeer1Org2() {
+setGlobalsForPeer1Org1(){
     export CORE_PEER_LOCALMSPID="Org2MSP"
-    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG2_CA
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/crypto-config/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
     export CORE_PEER_ADDRESS=localhost:10051
-
 }
 
-fetchChannelBlock() {
+createChannel(){
     rm -rf ./channel-artifacts/*
-    setGlobalsForPeer0Org2
+    setGlobalsForPeer0Org1
+    
     # Replace localhost with your orderer's vm IP address
-    peer channel fetch 0 ./channel-artifacts/$CHANNEL_NAME.block -o 10.40.41.163:7050 \
-        --ordererTLSHostnameOverride orderer.example.com \
-        -c $CHANNEL_NAME --tls --cafile $ORDERER_CA
+    peer channel create -o 10.40.41.163:7050 -c $CHANNEL_NAME \
+    --ordererTLSHostnameOverride orderer.example.com \
+    -f ./../../artifacts/channel/${CHANNEL_NAME}.tx --outputBlock ./channel-artifacts/${CHANNEL_NAME}.block \
+    --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA
 }
 
-# fetchChannelBlock
+# createChannel
 
-joinChannel() {
-    setGlobalsForPeer0Org2
+joinChannel(){
+    setGlobalsForPeer0Org1
     peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
-
-    setGlobalsForPeer1Org2
+    
+    setGlobalsForPeer1Org1
     peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
-
+    
 }
 
 # joinChannel
 
-updateAnchorPeers() {
-    setGlobalsForPeer0Org2
+updateAnchorPeers(){
+    setGlobalsForPeer0Org1
     # Replace localhost with your orderer's vm IP address
-    peer channel update -o 10.40.41.163:7050 --ordererTLSHostnameOverride orderer.example.com \
-        -c $CHANNEL_NAME -f ./../../artifacts/channel/${CORE_PEER_LOCALMSPID}anchors.tx \
-        --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA
-
+    peer channel update -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com -c $CHANNEL_NAME -f ./../../artifacts/channel/${CORE_PEER_LOCALMSPID}anchors.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA
+    
 }
 
 # updateAnchorPeers
 
-fetchChannelBlock
+# removeOldCrypto
+
+createChannel
 joinChannel
 updateAnchorPeers
